f364d01e9f58ddf2186b9626765918947a9b57b1
Gentur Waskito <genturwt@gmail.com>
2016-05-12 13:45:30 +0700

    add app front interface

---
+++ server.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var mongoose = require('mongoose');
+
+var app = require('./app/');
+
+function start() {
+  app.set('port', 3000);
+
+  var server = http.createServer(app);
+  server.listen(3000);
+
+  console.log('API is running on port', 3000);
+}
+
+mongoose.connect('mongodb://localhost/sale-stock');
+mongoose.connection.on('connected', start);
